4ce7e441f73b4694d44d805643364b4c977d3e3c
Gilmar Alberto <gilabreu00@gmail.com>
2022-11-26 19:34:29 -0300

    Criação do desafio4

--- aula06a.py
+++ aula06a.py
@@ -1,3 +1,3 @@
 n1=int(input("Digite n1: "))
 n2=int(input("Digite n2: "))
-print("A soma entre {0} e {1} vale {2}" .format(n1, n2, n1+n2))
+print(f"A soma entre {n1} e {n2} vale {n1+n2}")
---
+++ desafio004.py
@@ -0,0 +1,20 @@
+""" Faça um programa que leia algo pelo teclado
+    e mostre na tela o seu tipo primitivo
+    e todas as informações possíveis sobre ele"""
+
+str = input("Digite algo: ")
+
+print(type(str))
+
+print(f"É alfanumérico  (isalnum)      : {str.isalnum()}")
+print(f"É alfabético    (isalpha)      : {str.isalpha()}")
+print(f"É ascII         (isascii)      : {str.isascii()}")
+print(f"É tudo dígito   (isdigit)      : {str.isdigit()}")
+print(f"É tudo espaço   (isspace)      : {str.isspace()}")
+print(f"É tudo número   (isdecimal)    : {str.isdecimal()}")
+print(f"É identificador (is identifier): {str.isidentifier()}")
+print(f"É caixa baixa   (islower)      : {str.islower()}")
+print(f"É numérico      (isnumeric)    : {str.isnumeric()}")
+print(f"É imprimível    (isprintable)  : {str.isprintable()}")
+print(f"É titulado      (istitle)      : {str.istitle()}")
+print(f"É caixa alta    (isupper)      : {str.isupper()}")
