0735c49b94c3d52e64be08df5fbd5c3d9dbcee34
Gilmar Alberto <gilabreu00@gmail.com>
2023-05-06 18:31:54 -0300

    python guanabara

--- desafio004.py
+++ desafio004.py
@@ -1,6 +1,7 @@
 """ Faça um programa que leia algo pelo teclado
-    e mostre na tela o seu tipo primitivo
-    e todas as informações possíveis sobre ele"""
+        e mostre na tela o seu tipo primitivo
+        e todas as informações possíveis sobre ele
+"""
 
 str = input("Digite algo: ")
 
